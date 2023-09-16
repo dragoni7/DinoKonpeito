d574385ebf5c3c3b52d27c4dae6fb92e18c6a33f
Dragoni <90075917+dragoni7@users.noreply.github.com>
2023-09-16 15:10:34 -0700

    Added simple axis movement. Added Menu scene

--- PlayerController.cs
+++ PlayerController.cs
@@ -3,42 +3,37 @@ using System;
 
 public partial class PlayerController : CharacterBody2D
 {
-	public const float Speed = 700.0f;
-	public const float JumpVelocity = -700.0f;
+	[Export]
+	public float Speed { get; set; } = 300f;
 
-	// Get the gravity from the project settings to be synced with RigidBody nodes.
-	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
-
-    public override void _Ready()
-    {
-        base._Ready();
-    }
-
+    // Get the gravity from the project settings to be synced with RigidBody nodes.
+    public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
     public override void _PhysicsProcess(double delta)
 	{
 		Vector2 velocity = Velocity;
 
-		// Add the gravity.
+		// Falling
 		if (!IsOnFloor())
 			velocity.Y += gravity * (float)delta;
 
-		// Handle Jump.
-		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
-			velocity.Y = JumpVelocity;
+		// Handle Extend.
+		if (Input.IsActionPressed("Up"))
+			GD.Print("Extended");
 
 		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-		if (direction != Vector2.Zero)
+		float distance = Input.GetAxis("Left", "Right");
+
+		if (distance != 0f)
 		{
-			velocity.X = direction.X * Speed;
+			velocity.X = distance * Speed;
 		}
 		else
 		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+			velocity.X = Mathf.MoveToward(distance, 0, Speed);
 		}
 
 		Velocity = velocity;
+
 		MoveAndSlide();
 	}
 }
